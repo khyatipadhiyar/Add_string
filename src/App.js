c2a383f7e2382b440282eee5ec9385bf7e5747b6
Khyati Desai <khyati11892@gmail.com>
2021-09-25 19:15:41 +0530

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [sumstate, setSumstate] = useState(0);
+  const [inputString, setInputString]= useState('');
+
+  const addstring =(inputString)=>{
+    console.log(inputString)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Technical Assessment</h1>
+      <div>
+        
+        <input type="text" placeholder="Enter a string" onChange={(e)=>{setInputString(e.target.value)}}/>
+        <button onClick={()=>{addstring(inputString)}}>calculation</button>
+        <br/>
+        <br/>
+        <label>sum = {sumstate}</label>
+      </div>
     </div>
   );
 }
